6e4128a95004ededd7672624e54b6602e50e7662
dsvezh <144603979+dsvezh@users.noreply.github.com>
2023-09-16 16:54:36 +0300

    Update Main.java

--- src/main/java/ru/svezhik97/Main.java
+++ src/main/java/ru/svezhik97/Main.java
@@ -1,11 +1,11 @@
 package ru.svezhik97;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code!!!!!!!
+// then press Enter. You can now see whitespace characters in your code!
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how blablabla
-        // IntelliJ IDEA suggests fixing it.
+        // Press Alt+Enter with your caret at the highlighted qwerty123
+        // IntelliJ IDEA suggests fixing it...
         System.out.printf("Hello qa.guru!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
